c1bf0c1d6a854c06366db4b16fabcf7b72a55b8c
Vasiliy B <interlocked4080@gmail.com>
2021-06-07 00:18:18 +0300

    first commit

---
+++ cucumber_bot.py
@@ -0,0 +1,64 @@
+import json
+import random
+import requests
+import vk_api
+from vk_api.utils import get_random_id
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from cucumber_commands import commands, command_list
+import bs4
+from timeit import default_timer as timer
+
+class bot_main(object):
+
+    def __init__(self, user_id):
+        self.user_id = user_id
+
+    @staticmethod
+    def get_vk_token(flag=True):
+        if flag is True:
+            with open('config.json', 'r') as f:
+                data = json.load(f)
+            token = data['token']
+            f.close()
+            return token
+        else:
+            return 'none'
+
+    @staticmethod
+    def cucumber_auth():
+        auth = vk_api.VkApi(token=bot_main.get_vk_token(flag=True))
+        return auth
+
+    @staticmethod
+    def msg_write(chat_id, msg):
+        bot_main.cucumber_auth().method('messages.send', {'chat_id': chat_id, 'message': msg, 'random_id': get_random_id()})
+
+    @staticmethod
+    def arg_split(text):
+        return text.split()[1:]
+
+    @staticmethod
+    def on_msg(message):
+        if message == command_list[0]:
+            return commands.command_punkname()
+        if message == '{0} {1}'.format(command_list[1], ' '.join(bot_main.arg_split(message))):
+            city = ' '.join(bot_main.arg_split(message))
+            return commands.command_weather(city=city)
+        if message == command_list[2]:
+            return 'чё приветкаешь?'
+        if message == command_list[3]:
+            return commands.command_help()
+
+
+
+lp = VkBotLongPoll(bot_main.cucumber_auth(), group_id=197949409)
+vk = bot_main.cucumber_auth().get_api()
+
+for event in lp.listen():
+    if event.type == VkBotEventType.MESSAGE_NEW and event.from_chat:
+        try:
+            rm = event.message.get('text')
+            sender = event.chat_id
+            bot_main.msg_write(sender, bot_main.on_msg(rm))
+        except vk_api.ApiError:
+            pass
---
+++ cucumber_commands.py
@@ -0,0 +1,57 @@
+import datetime
+import json
+import random
+import requests
+import vk_api
+from vk_api.utils import get_random_id
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+import bs4
+from timeit import default_timer as timer
+
+command_list = 'кликуха погода привет помощь'.split()
+
+const_api_key = '65de895f37ea08ee2a9c4d856de58897'
+
+class commands(object):
+
+    @staticmethod
+    def command_punkname():
+        with open('nicks.txt', 'r', encoding='utf-8') as k:
+            nicks = k.readlines()
+        with open('names.txt', 'r', encoding='utf-8') as n:
+            names = n.readlines()
+        return '{0} {1}'.format(random.choice(names).replace('\n', ''), random.choice(nicks).replace('\n', ''))
+
+    @staticmethod
+    def command_weather(city):
+
+        try:
+            url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&APPID={const_api_key}'
+
+            data = requests.get(url).json()
+
+            temp = data['main']['temp']
+
+            r_name = data['name']
+
+            region = data['sys']['country']
+
+            speed = data['wind']['speed']
+
+            condition = data['weather'][0]['main']
+
+            message_string = f'Погода в {r_name} {datetime.datetime.utcnow()} ' \
+                             f'\n Страна: {region} ' \
+                             f'\n Температура: {round(int(temp))} C ' \
+                             f'\n Состояние: {condition} ' \
+                             f'\n Скорость ветра: {speed} км/ч'
+            return message_string
+        except Exception:
+            return 'Город не найден!'
+
+    @staticmethod
+    def command_help():
+        output = ''
+        for j in range(0, len(command_list)):
+            output += '\n' + command_list[j]
+        return 'Список комманд: {0}'.format(output)
